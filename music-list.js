d5b02ab8caeab27b425543715512808419436484
Premkumar Savale <138439572+premkumarsavale12@users.noreply.github.com>
2023-07-17 12:01:05 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,62 @@
+let allMusic = [
+  {
+    name: "Dil Galti Kar Baitha Hai",
+    artist: "Jubin Nautiyal",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Jhoome Jo Pathaan",
+    artist: "Arijit Singh",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Apna Bana Le",
+    artist: "Arijit Singh",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Kesariya (Brahmastra)",
+    artist: "Arijit Singh",
+    img: "music-7",
+    src: "music-7"
+  }
+];
